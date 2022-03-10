66883ed53b2a78d4f321ddf1eb1586dd84c73a1c
johgee <graceeunaejoh@grace.com>
2022-03-09 19:49:29 -0500

    name and number not working

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,2 +1,6 @@
 class ParamsExamplesController < ApplicationController
+  def query_params 
+    input_name = params["name"]
+    render json: {message: "your name is #{input_name}", message2: "your number is #{params['x']}"}
+  end
 end
