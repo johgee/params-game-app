95d1b011da91dbc16604512c714c301099554c26
johgee <graceeunaejoh@grace.com>
2022-03-09 21:38:16 -0500

    name game and number game

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -8,4 +8,22 @@ class ParamsExamplesController < ApplicationController
     enter_name = params["a"]
     render json: {message: "your name starts with the first letter of the alphabet!"}
   end
+
+  def number_game
+    user_number = params["user_number"].to_i
+    winning_number = 19
+    if user_number < winning_number
+      output = "guess higher"
+    elsif user_number > winning_number 
+      output = "guess lower"
+    else 
+      output = "you win!"
+    end 
+    render json: {user_number: output}
+  end 
+
+  def url_segment
+    input_value = params["title"]
+    render json: {message: "the url is #{input_value}"}
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "/query_params" => "params_examples#query_params"
   get "/enter_name" => "params_examples#enter_name"
+  get "/number_game" => "params_examples#number_game"
+  get "/url_segment_params/:title" => "params_examples#url_segment"
 end
