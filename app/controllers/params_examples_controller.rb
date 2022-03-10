0fb2552a21b05b08e8f716c18c03ebcbe73b544d
johgee <graceeunaejoh@grace.com>
2022-03-09 20:20:39 -0500

    adding what i have

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -3,4 +3,9 @@ class ParamsExamplesController < ApplicationController
     input_name = params["name"]
     render json: {message: "your name is #{input_name}", message2: "your number is #{params['x']}"}
   end
+
+  def enter_name
+    enter_name = params["a"]
+    render json: {message: "your name starts with the first letter of the alphabet!"}
+  end
 end
